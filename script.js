870c9eef168c38d100109d00178e783fcaca6b2c
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2021-12-05 18:58:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+console.log(txtInput.value);
+var serverURL = "https://api.funtranslations.com/translate/Pirate.json"
+function getTranslationUrl(text)
+{
+  return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error)
+{
+  console.log("Error occured" , error);
+  alert("Something wrong with the server: try again after sometime");
+}
+
+function clickHandler()
+{
+    var inputText = txtInput.value;
+  fetch(getTranslationUrl(inputText))
+  .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    }) 
+  .catch(errorHandler)
+};
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
+
+
+
